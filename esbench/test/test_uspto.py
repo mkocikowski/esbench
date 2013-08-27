121bad4d4daf02b8f5d5b292cb589c61523fdd0c
Mik Kocikowski <mkocikowski@gmail.com>
2013-08-27 13:13:50 -0700

    Complete record parsing.

--- esbench/test/test_uspto.py
+++ esbench/test/test_uspto.py
@@ -3,10 +3,12 @@
 import datetime
 import os.path
 import unittest
+import json
 
 import esbench.uspto
 
 ASSIGNMENT = """<patent-assignment><assignment-record><reel-no>31066</reel-no><frame-no>972</frame-no><last-update-date><date>20130824</date></last-update-date><purge-indicator>N</purge-indicator><recorded-date><date>20130823</date></recorded-date><page-count>4</page-count><correspondent><name>OTIS PATENT LAW</name><address-1>1181 WADE STREET</address-1><address-2>HIGHLAND PARK, IL 60035</address-2></correspondent><conveyance-text>ASSIGNMENT OF ASSIGNORS INTEREST (SEE DOCUMENT FOR DETAILS).</conveyance-text></assignment-record><patent-assignors><patent-assignor><name>MOYER, ILAN</name><execution-date><date>20110526</date></execution-date></patent-assignor><patent-assignor><name>LOBOVSKY, MAXIM</name><execution-date><date>20110526</date></execution-date></patent-assignor></patent-assignors><patent-assignees><patent-assignee><name>MASSACHUSETTS INSTITUTE OF TECHNOLOGY</name><address-1>77 MASSACHUSETTS AVE.</address-1><city>CAMBRIDGE</city><state>MASSACHUSETTS</state><postcode>02139</postcode></patent-assignee></patent-assignees><patent-properties><patent-property><document-id><country>US</country><doc-number>13116504</doc-number><kind>X0</kind><date>20110526</date></document-id><document-id><country>US</country><doc-number>20110289741</doc-number><kind>A1</kind><date>20111201</date></document-id><invention-title lang="en">Methods and apparatus for applying tension to a motion transmission element</invention-title></patent-property></patent-properties></patent-assignment>"""
+PARSED = """{"assignment_record": {"conveyance_text": "ASSIGNMENT OF ASSIGNORS INTEREST (SEE DOCUMENT FOR DETAILS).", "correspondent": {"address": {"address_1": "1181 WADE STREET", "address_2": "HIGHLAND PARK, IL 60035", "address_3": "", "address_4": "", "city": "", "country_name": "", "postcode": "", "state": ""}, "name": "OTIS PATENT LAW"}, "date_last_update": "2013-08-24", "date_recorded": "2013-08-23", "frame_no": "972", "reel_no": "31066"}, "patent_assignees": [{"address": {"address_1": "77 MASSACHUSETTS AVE.", "address_2": "", "address_3": "", "address_4": "", "city": "CAMBRIDGE", "country_name": "", "postcode": "02139", "state": "MASSACHUSETTS"}, "name": "MASSACHUSETTS INSTITUTE OF TECHNOLOGY"}], "patent_assignors": [{"address": {"address_1": "", "address_2": "", "address_3": "", "address_4": "", "city": "", "country_name": "", "postcode": "", "state": ""}, "date_execution": "2011-05-26", "name": "MOYER, ILAN"}, {"address": {"address_1": "", "address_2": "", "address_3": "", "address_4": "", "city": "", "country_name": "", "postcode": "", "state": ""}, "date_execution": "2011-05-26", "name": "LOBOVSKY, MAXIM"}], "patent_properties": [{"document-ids": [{"_type_expected": "APP", "_type_inferred": "APP", "country": "US", "date": "2011-05-26", "doc_number": "13116504", "kind": "X0"}, {"_type_expected": "PUB", "_type_inferred": "PUB", "country": "US", "date": "2011-12-01", "doc_number": "20110289741", "kind": "A1"}], "invention-title": "METHODS AND APPARATUS FOR APPLYING TENSION TO A MOTION TRANSMISSION ELEMENT", "invention-title-language": "EN"}]}"""
 
 class USPTOTest(unittest.TestCase):
 
@@ -28,6 +30,7 @@ class USPTOTest(unittest.TestCase):
             (datetime.date.today().strftime(r'http://storage.googleapis.com/patents/assignments/2013/ad%Y%m%d.zip'),
              datetime.date.today().strftime(r'ad%Y%m%d.zip')), 
             list(esbench.uspto.urls())[0])
+        self.assertLess(len(list(esbench.uspto.urls(400))), 365)
 
 #     def test_download_and_unzip(self): 
 #         for url, fn in esbench.uspto.urls(4):
@@ -46,8 +49,12 @@ class USPTOTest(unittest.TestCase):
     def test_parse(self):
         parsed = list(esbench.uspto.parse(ASSIGNMENT))[0]
         self.assertTrue(isinstance(parsed, dict))
-        
-        
+
+
+    def test_PatentAssignment(self):
+        ar = esbench.uspto.PatentAssignment(ASSIGNMENT)
+        s = json.dumps(ar, indent=None, sort_keys=True)        
+        self.assertEqual(s, PARSED)
         
 if __name__ == "__main__":
     unittest.main()     
--- esbench/uspto.py
+++ esbench/uspto.py
@@ -8,6 +8,7 @@ import argparse
 import sys
 import collections
 import json
+import datetime
 from xml.etree import ElementTree
 
 import requests
@@ -17,6 +18,14 @@ __version__ = (0,0,1)
 
 logger = logging.getLogger(__name__)
 
+def format_date(s):
+    try: 
+        dt = datetime.datetime.strptime(s, r"%Y%m%d")
+        return dt.strftime(r"%Y-%m-%d")
+    except ValueError: 
+        return ""
+
+
 def urls(count=1):
     d = datetime.datetime.utcnow()
     day = datetime.timedelta(days=1)
@@ -24,6 +33,8 @@ def urls(count=1):
         s = d.strftime(r'ad%Y%m%d.zip')
         yield ("http://storage.googleapis.com/patents/assignments/2013/%s" % s, s)
         d -= day
+        if d < datetime.datetime(2013, 01, 01): 
+            break
 
 
 def download(url, fn):
@@ -89,17 +100,23 @@ def parse(s):
         
 
 
-class Element(dict):
+class PatentAssignmentElement(dict):
 
     def __init__(self, root): 
-        dict.__init__(self) 
+        dict.__init__(self)
+#         print(type(root)) 
         self.from_xml(root)
+        
+    def __setitem__(self, key, value):
+        if isinstance(value, str):
+            value = value.upper()
+        dict.__setitem__(self, key, value)
 
     def from_xml(self, root):
         raise NotImplementedError()
 
 
-class PatentProperty(Element): 
+class PatentProperty(PatentAssignmentElement): 
 
     def from_xml(self, root):
     
@@ -125,16 +142,15 @@ class PatentProperty(Element):
                 self['document-ids'].append(DocumentID(docid))
 
 
-class DocumentID(Element):
+class DocumentID(PatentAssignmentElement):
 
     APPLICATION = 'app'
     PATENT = 'pat'
     PUBLICATION = 'pub'
     
-    FIELDS = ['country', 'doc-number', 'kind', 'date', '_type_expected', '_type_inferred']
-
     def _type(self):
-        idstr = self['doc-number']
+    
+        idstr = self['doc_number']
         if len(idstr) == 7: 
             try: int(idstr, 10)
             except ValueError: return False
@@ -156,12 +172,11 @@ class DocumentID(Element):
 
     
     def from_xml(self, root):
-        for k in self.FIELDS:
-            try: 
-                text = root.findtext(k)
-                self[k] = text.strip() if text else ''
-            except AttributeError as exc:
-                pass
+    
+        self['country'] = root.findtext('./country', default='')
+        self['doc_number'] = root.findtext('./doc-number', default='')
+        self['kind'] = root.findtext('./kind', default='')
+        self['date'] = format_date(root.findtext('./date', default=''))
         self._type()
         
 
@@ -186,22 +201,95 @@ class PublicationNumber(DocumentID):
         self['_type_expected'] = DocumentID.PUBLICATION
 
 
+class Address(PatentAssignmentElement):
+
+    def from_xml(self, root):
+        self['address_1'] = root.findtext('.//address-1', default='')
+        self['address_2'] = root.findtext('.//address-2', default='')
+        self['address_3'] = root.findtext('.//address-3', default='')
+        self['address_4'] = root.findtext('.//address-4', default='')
+        self['city'] = root.findtext('.//city', default='')
+        self['state'] = root.findtext('.//state', default='')
+        self['country_name'] = root.findtext('.//country-name', default='')
+        self['postcode'] = root.findtext('.//postcode', default='')
+
+
+class Entity(PatentAssignmentElement):
+
+    def from_xml(self, root):
+        self['name'] = root.findtext('./name', default='')
+        self['address'] = Address(root)
+
+
+class Assignor(Entity): 
+
+    def from_xml(self, root):
+        super(Assignor, self).from_xml(root)
+        self['date_execution'] = format_date(root.findtext('./execution-date/date', default=''))
+
+
+class Assignee(Entity):
+    pass
+
+
+class Correspondent(Entity):
+    pass
+    
+
+class AssignmentRecord(PatentAssignmentElement):
+        
+    def from_xml(self, root):
+        self['reel_no'] = root.findtext('./reel-no', default='')
+        self['frame_no'] = root.findtext('./frame-no', default='')
+        self['date_last_update'] = format_date(root.findtext('./last-update-date/date', default=''))
+        self['date_recorded'] = format_date(root.findtext('./recorded-date/date', default=''))
+        self['correspondent'] = Correspondent(root.find('./correspondent'))
+        self['conveyance_text'] = root.findtext('./conveyance-text', default='')
+
+
+
+class PatentAssignment(dict):
+    
+    def __init__(self, xml): 
+        dict.__init__(self)
+        self.xml = xml
+        self.tree = ElementTree.fromstring(self.xml)
+        self['assignment_record'] = AssignmentRecord(self.tree.find('.//assignment-record'))
+        self['patent_assignors'] = []
+        for node in self.tree.iterfind('.//patent-assignor'):
+            a = Assignor(node)
+            self['patent_assignors'].append(a)
+        self['patent_assignees'] = []
+        for node in self.tree.iterfind('.//patent-assignee'):
+            a = Assignee(node)
+            self['patent_assignees'].append(a)
+        self['patent_properties'] = []
+        for node in self.tree.iterfind('.//patent-property'):
+            p = PatentProperty(node)
+            self['patent_properties'].append(p)
+
+
 def get_args_parser():
+
     parser = argparse.ArgumentParser(description="esbench USPTO patent assignment downloader.")
     parser.add_argument('-v', '--version', action='version', version=__version__)
     parser.add_argument('-d', '--days', type=int, default=3, help="fetch records for how many days? (default: %(default)s)")
+    parser.add_argument('-i', metavar='INDENT', type=int, default=None, help="if set, format JSON output with indent (default: %(default)s)")
     return parser
 
 
 def main():
+
     logging.basicConfig(level=logging.WARNING)
     args = get_args_parser().parse_args()
 
     try: 
-        for a in get_assignments(args.days):
-            for p in parse(a):
-                print(json.dumps(p))
-#                 sys.exit(1)
+        for line in get_assignments(args.days):
+#             print(line)
+            a = PatentAssignment(line)
+            print(json.dumps(a, indent=args.i, sort_keys=True))
+#             print("*********************************************************")
+#             sys.exit(1)
 
     except IOError:
         pass
