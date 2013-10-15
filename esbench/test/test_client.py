293bafe24769b61ef9d7142947bdd69c23d346a3
Mik Kocikowski <mkocikowski@gmail.com>
2013-10-15 07:08:34 -0700

    Undoing 7df4514f
    The problem was when running unit tests with relative path. When run
    as a module, there is no problem (this is all for travis integration).

--- esbench/test/test_client.py
+++ esbench/test/test_client.py
@@ -29,8 +29,7 @@ class ClientTest(unittest.TestCase):
                 'observations': 10, 
                 'data': None, 
                 'append': False, 
-#                 'config_file_path': os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", "config.json")),
-                'config_file_path': args.config_file_path, # cheating
+                'config_file_path': os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", "config.json")),
             }
         )
     
